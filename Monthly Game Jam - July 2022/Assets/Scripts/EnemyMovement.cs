c208a55272ac20bb2c0761de084c7ec0c5012db1
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-07-24 00:45:05 -0500

    feat - Enemies target player

---
+++ Monthly Game Jam - July 2022/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+    public Transform target;
+
+    Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+}
