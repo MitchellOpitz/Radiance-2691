48cc770386703d84f3e855d0783447a4bcb8315a
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-07-23 23:38:05 -0500

    feat - Add player movement

---
+++ Monthly Game Jam - July 2022/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+        rb.rotation = angle;
+    }
+}
